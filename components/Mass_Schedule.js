a6e5f44dfd8c05d064a4df1a1984c8206ad6e52f
frankesomcare <frankline.wakasiaka@esomcare.com>
2023-12-05 19:10:26 +0300

    mass times

--- components/Mass_Schedule.js
+++ components/Mass_Schedule.js
@@ -2,9 +2,41 @@ import React from 'react'
 
 function MassSchedule() {
   return (
-    <div>
-        <h1 className='text-primary'>Mass Schedule</h1>
+    <div className="flex flex-col md:flex-row md:mx-24">
+    <div className="md:w-1/2 p-4 "> {/* Adjust the width and padding as needed */}
+      {/* Content for the first column */}
+      <h1 className='mb-10'>Mass Times.</h1>
+      <div>
+        <h2>Tuesday-Friday</h2>
+        <h3>7:30 am & 6:00pm</h3>
+        <h2>Monday & Saturday</h2>
+        <h3>7:30 am </h3>
+        <h2>Sunday</h2>
+        <h3>7:30 am, 9:30 am, 10:30 am, 12:00 noon </h3>
+      </div>
+      <h1 className='my-10'>Confessions.</h1>
+      <div>
+        <h2>Tuesday-Friday</h2>
+        <h3>5:30 pm </h3>
+        <h2>Sunday</h2>
+        <h3>30 minutes before Mass</h3>
     </div>
+    </div>
+    <div className="md:w-1/2 p-4"> {/* Adjust the width and padding as needed */}
+      {/* Content for the second column */}
+      <h1 className='mb-10'>Eucharistic Adoration</h1>
+      <div>
+        <h3>In the Church:</h3>
+        <h2>Monday-Saturday</h2>
+        <h3>7:00 am </h3>
+
+      </div>
+      <h1 className='my-10'>Holy Rosary</h1>
+      <h2>Tuesday-Friday</h2>
+        <h3>5:30 pm </h3>
+
+    </div>
+  </div>
   )
 }
 
