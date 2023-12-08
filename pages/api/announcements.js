c50bed0b9b375db9de16eafbda2c3d75f3286408
frankesomcare <frankline.wakasiaka@esomcare.com>
2023-12-08 22:25:46 +0300

    sunday id name

--- pages/api/announcements.js
+++ pages/api/announcements.js
@@ -1,12 +1,12 @@
 import mongooseConnect from "@/lib/mongoose";
 import Announcement from "@/models/Announcements";
 import { NextResponse } from "next/server";
-
+import Sunday from "@/models/Sunday"; 
 export default async function handler(req, res) {
   
   try {
     await mongooseConnect();
-    const announcements = await Announcement.find()
+    const announcements = await Announcement.find({}).populate('sunday','sunday',Sunday)
     res.json(announcements);
   } catch (error) {
     console.error('Error fetching Announcements:', error);
