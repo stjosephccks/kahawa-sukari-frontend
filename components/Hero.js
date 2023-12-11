9b2fe826255a053ec5c6fb7358c7e96bc959934a
frankesomcare <frankline.wakasiaka@esomcare.com>
2023-12-11 13:10:52 +0300

    image

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero({imageUrl, text}) {
   return (
-    <div className="relative h-auto md:h-64 flex items-center justify-center">
+    <div className="relative h-auto md:h-80 flex items-center justify-center">
       <img
         className="object-cover w-full h-full"
         src={imageUrl}
