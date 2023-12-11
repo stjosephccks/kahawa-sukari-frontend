015b30695f823a86e481c67054cd03d3620bf237
frankesomcare <frankline.wakasiaka@esomcare.com>
2023-12-11 12:50:27 +0300

    image

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar";
 
 
 export default function  Home(){
-  const imageurl ='https://scontent-mba1-1.xx.fbcdn.net/v/t39.30808-6/309154881_418748297054427_2476714163710300533_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=783fdb&_nc_ohc=WHlFZbZKqEkAX-UYgb0&_nc_ht=scontent-mba1-1.xx&oh=00_AfAugd7YpQDpwMPLwdA57EkGJ9gpBJIv37QKRK44kjNTjQ&oe=6574771C'
+  const imageurl ='https://kahawa-sukari.s3.amazonaws.com/stJosephkahawasukari.jpg'
   const text ='We are born to love, we live to love, and we will die to love still more.'
   return (
     <Layout>
