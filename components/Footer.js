13e28cc3d96c8c2fbc84f2e852559ff06e416eb7
frankesomcare <frankline.wakasiaka@esomcare.com>
2023-12-14 17:13:19 +0300

    other pages pseudo

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Footer() {
+const [isVisible, setIsVisible]= useState(false)
+  useEffect(() => {
+    const toggleVisibility = () => {
+      // if the user scrolls down, show the button
+      window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false)
+    }
+    // listen for scroll events
+    window.addEventListener("scroll", toggleVisibility)
+
+    // clear the listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility)
+    }
+  }, [])
+
+  // handles the animation when scrolling to the top
+  const scrollToTop = () => {
+    isVisible &&
+      window.scrollTo({
+        top: 0,
+        behavior: "auto",
+      })
+  }
   return (
     <div className="bg-primary h-72 flex items-center justify-center">
+        
+        
         <div className='text-center text-white'>
+        <div className={`flex items-center justify-center cursor-pointer transition-opacity duration-200 ${
+        isVisible ? "opacity-100" : "opacity-0"
+      }`}  onClick={scrollToTop}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>&nbsp;Back to Top</span>
+      </div>
+      
         <h3 className='my-2'>ST.JOSEPH CATHOLIC CHURCH KAHAWA SUKARI</h3>
             <h4 className='mb-2'>CHURCH OFFICE HOURS</h4>
             <p className=''><span className='font-bold'>Monday-Friday:</span> 8:00am - 5:00pm</p>
