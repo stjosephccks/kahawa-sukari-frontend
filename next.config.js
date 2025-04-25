9890134492a4c256b2b183dd294af841e2ff120c
frankesomcarem <wakasiakafrankline@gmail.com>
2025-04-25 23:38:41 +0300

    eslint ignorew

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
-  images:{
+  images: {
     remotePatterns: [
       {
-        protocol:'https',
-        hostname:'kahawa-sukari.s3.amazonaws.com',
+        protocol: "https",
+        hostname: "kahawa-sukari.s3.amazonaws.com",
       },
-      {protocol:'https',
-    hostname:'https://kahawa-sukari-next.s3.ap-south-1.amazonaws.com'}
-      ]},
-  
-}
+      {
+        protocol: "https",
+        hostname: "https://kahawa-sukari-next.s3.ap-south-1.amazonaws.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
