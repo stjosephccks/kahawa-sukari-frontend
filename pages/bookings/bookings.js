fbfe963ef369fa5df3e9d9edae4197b714089828
frankesomcarem <wakasiakafrankline@gmail.com>
2025-06-04 18:46:17 +0300

    ----pre superbase

---
+++ pages/bookings/bookings.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+function Bookings() {
+    return (
+        <Layout>
+            <h1>Bookings</h1>
+        </Layout>
+    );
+}
+
+export default Bookings;
