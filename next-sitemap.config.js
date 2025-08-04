d7f42485144a874163864bd39e547979594be89c
frankline <franklinewax@gmail.com>
2025-08-04 16:21:48 +0300

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
+  // Disable static sitemap generation since we're using a dynamic approach
+  generate: false,
   siteUrl: 'https://stjosephchurchkahawasukari.org',
   generateRobotsTxt: true,
   generateIndexSitemap: true,
